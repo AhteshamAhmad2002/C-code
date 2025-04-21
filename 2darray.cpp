f3999cc7a3e4c07f1c1960dadf3121b1b19262e4
AhteshamAhmad2002 <ahteshamahmad462@gmail.com>
2025-04-21 18:27:18 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int matrix[4][3] = {{1, 2, 3} , {4, 5, 6} , {7, 8, 9}, {10, 11, 12}};
+    int row = 4;
+    int col = 3;
+    int key = 8;
+
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            if(matrix[i][j] == key){
+                cout<<"element found is "<<endl;
+            }else{
+                cout<<"Not Found";
+            }
+        }
+    }
+    return 0;
+}
